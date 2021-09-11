c4233628ef375bf238b3349a3ea0d2f054285817
Master <vtwoguru@gmail.com>
2021-09-11 23:06:46 +0900

    get Nth Pibonacci

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,39 @@ use rand::Rng;
 use std::cmp::Ordering;
 
 fn main() {
+    // guessing();
+    loop {
+        println!("Please enter the N to get Nth pibonacci number.");
+        let mut index = String::new();
+        io::stdin()
+            .read_line(&mut index)
+            .expect("Failed to read line.");
+        let index: u32 = match index.trim().parse() {
+            Ok(n) => n,
+            Err(_) => {
+                println!("Please enter number.not string");
+                continue;
+            },
+        };
+        let mut counter = 1;
+        let mut i_2: u32 = 1;
+        let mut i_1: u32 = 1;
+        let mut i: u32 = 0;
+        let result = loop {
+            counter += 1;
+            i = i_1 + i_2;
+            i_1 = i_2;
+            i_2 = i;
+            if counter == index - 1 {
+                break i;
+            }
+        };
+
+        println!("The nth number is {}!", result);
+    }
+}
+
+fn guessing() {
     println!("Let's guess the number!");
 
     loop {
