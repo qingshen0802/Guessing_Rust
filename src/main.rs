9e443fb2384c7d1166b21b8f4bb001ed2acd787e
Master <vtwoguru@gmail.com>
2021-09-11 16:36:37 +0900

    finish

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Let's guess the number!");
+
+    loop {
+        let rand_number = rand::thread_rng().gen_range(1..=10);
+
+        println!("Enter the number you thought");
+
+        let mut number = String::new();
+        io::stdin()
+            .read_line(&mut number)
+            .expect("Failed to read line");
+        println!("You guessed {}", number);
+
+        let number: u32 = match number.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("It's not number!");
+                continue;
+            },
+        };
+
+        match number.cmp(&rand_number) {
+            Ordering::Equal => {
+                println!("Perfect!");
+                break;
+            },
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too Big!"),
+        }
+
+        println!("The correct answer is {}", rand_number);
+    }
 }
